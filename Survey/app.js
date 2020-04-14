0a3d92045a27b32b983cf3b0a9c8cfde3e859c30
Briseid Pagador <bpagador@gmail.com>
2020-04-14 15:53:45 -0700

    start using local storage

--- Survey/app.js
+++ Survey/app.js
@@ -1,4 +1,5 @@
-import findById from './Common/utils.js';
+import { randomSelection } from '../Common/utils.js';
+
 
 const image1 = document.getElementById('image-1');
 const image2 = document.getElementById('image-2');
@@ -8,3 +9,21 @@ const input1 = document.getElementById('option-1');
 const input2 = document.getElementById('option-2');
 const input3 = document.getElementById('option-3');
 
+let images = [image1, image2, image3];
+let inputs = [input1, input2, input3];
+
+const finalState = [];
+const selectionsStringified = JSON.stringify(finalState);
+localStorage.setItem('ANY-DUPLICATE', selectionsStringified);
+
+export const chooseProduct = (array) => {
+    const currentState = [];
+
+    let stringSelections = localStorage.getItem('ANY-DUPLICATE');
+    let parsedSelections = JSON.parse(stringSelections);
+    
+    for (let i = 0; i < 3; i++) {
+        let number = randomSelection ();
+
+    }
+}
--- Survey/survey.js
+++ Survey/survey.js
@@ -1,9 +1,22 @@
-export class NoveltyProducts {
-    constructor(products) {
-        this.products = products.slice();
-    }
+import surveyProducts from '../Common/products-data.js';
 
-    getNoveltyProducts() {
-        return this.products;
+
+const nextButton = document.getElementById('next-button');
+let selection = 0;
+const resultsArray = [];
+const resultsArrayStringified = JSON.stringify(resultsArray);
+localStorage.setItem('RESULTS', resultArrayStringified);
+
+
+chooseProduct(surveyProducts);
+nextButton.addEventListener('click', () =>{
+    const userSelection = document.querySelector('input:checked');
+    incrementSelection(userSelection.value);
+    chooseProduct(surveyProducts);
+    selection++;
+
+    if (questions > 25) {
+        location.replace('../Results/results-page.html');
     }
-}
+});
+
