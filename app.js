08fff00433d78b3ee0470aea14491384965b85af
Briseid Pagador <bpagador@gmail.com>
2020-04-14 14:03:08 -0700

    add findById function and get started on DOM elements

---
+++ app.js
@@ -0,0 +1,10 @@
+import findById from './Common/utils.js';
+
+const image1 = document.getElementById('image-1');
+const image2 = document.getElementById('image-2');
+const image3 = document.getElementById('image-3');
+
+const input1 = document.getElementById('option-1');
+const input2 = document.getElementById('option-2');
+const input3 = document.getElementById('option-3');
+
