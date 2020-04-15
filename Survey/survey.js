ca1821c37138b4113e1b2fdc3d5414ea30c5e3a3
Briseid Pagador <bpagador@gmail.com>
2020-04-14 18:06:33 -0700

    fix submit button

--- Survey/survey.js
+++ Survey/survey.js
@@ -1,22 +1,22 @@
-import surveyProducts from '../Common/products-data.js';
+// import surveyProducts from '../Common/products-data.js';
 
 
-const nextButton = document.getElementById('next-button');
-let selection = 0;
-const resultsArray = [];
-const resultsArrayStringified = JSON.stringify(resultsArray);
-localStorage.setItem('RESULTS', resultArrayStringified);
+// const submitButton = document.getElementById('submit-button');
+// let selection = 0;
+// const resultsArray = [];
+// const resultsArrayStringified = JSON.stringify(resultsArray);
+// localStorage.setItem('RESULTS', resultArrayStringified);
 
 
-chooseProduct(surveyProducts);
-nextButton.addEventListener('click', () =>{
-    const userSelection = document.querySelector('input:checked');
-    incrementSelection(userSelection.value);
-    chooseProduct(surveyProducts);
-    selection++;
+// chooseProduct(surveyProducts);
+// nextButton.addEventListener('click', () =>{
+//     const userSelection = document.querySelector('input:checked');
+//     incrementSelection(userSelection.value);
+//     chooseProduct(surveyProducts);
+//     selection++;
 
-    if (questions > 25) {
-        location.replace('../Results/results-page.html');
-    }
-});
+//     if (questions > 25) {
+//         location.replace('../Results/results-page.html');
+//     }
+// });
 
